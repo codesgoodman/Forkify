d492e53a07013f9a9aaf5ec9948c6f70ac1228d2
Kihyun Kim <rlgus3689@gmail.com>
2023-02-15 21:44:06 +0900

    add guard clause for not being able to retrieve ID scenario

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -587,6 +587,7 @@ const renderSpinner = function(parentEl) {
 const showRecipe = async function() {
     try {
         const id = window.location.hash.slice(1);
+        if (!id) return;
         renderSpinner(recipeContainer);
         const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
