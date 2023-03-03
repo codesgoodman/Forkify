ba496f95a5c92292bbcab52561fad669b98b181d
Kihyun Kim <rlgus3689@gmail.com>
2023-03-03 23:49:35 +0900

    modify markup according to appropriate data

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -4,15 +4,15 @@ class ResultsView extends View {
   _generateMarkup() {
     return this._data.map(this._generateMarkupPreview).join('');
   }
-  _generateMarkupPreview() {
+  _generateMarkupPreview(result) {
     return `<li class="preview">
-      <a class="preview__link preview__link--active" href="#23456">
+      <a class="preview__link preview__link--active" href="#${result.id}">
         <figure class="preview__fig">
-          <img src="src/img/test-1.jpg" alt="Test" />
+          <img src="${result.image}" alt="Test" />
         </figure>
         <div class="preview__data">
-          <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
-          <p class="preview__publisher">The Pioneer Woman</p>
+          <h4 class="preview__title">${result.title}</h4>
+          <p class="preview__publisher">${result.publisher}</p>
           <div class="preview__user-generated">
             <svg>
               <use href="src/img/icons.svg#icon-user"></use>
