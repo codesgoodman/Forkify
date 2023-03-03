ace57037f860c24b2c718b345f84bafa72735fba
Kihyun Kim <rlgus3689@gmail.com>
2023-03-03 23:55:33 +0900

    change alt text to title

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -9,7 +9,7 @@ class ResultsView extends View {
     return `<li class="preview">
       <a class="preview__link" href="#${result.id}">
         <figure class="preview__fig">
-          <img src="${result.image}" alt="Test" />
+          <img src="${result.image}" alt="${result.title}" />
         </figure>
         <div class="preview__data">
           <h4 class="preview__title">${result.title}</h4>
