2c5e62a45f2c37d4e708165f0604bf5c49ca967a
Kihyun Kim <rlgus3689@gmail.com>
2023-03-11 22:33:36 +0900

    clear up console logs after implementation

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2049,7 +2049,6 @@ const loadSearchResults = async function(query) {
     try {
         state.search.query = query;
         const data = await (0, _helpersJs.getJson)(`${(0, _configJs.API_URL)}?search=${query}`);
-        console.log(data);
         state.search.results = data.data.recipes.map((rec)=>{
             return {
                 id: rec.id,
--- src/js/model.js
+++ src/js/model.js
@@ -37,7 +37,6 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJson(`${API_URL}?search=${query}`);
-    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
