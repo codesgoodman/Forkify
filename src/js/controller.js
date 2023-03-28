44870231672e07117198ebe39d8ee54de2467290
Kihyun Kim <rlgus3689@gmail.com>
2023-03-28 21:19:32 +0900

    implement marking of selected search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,8 @@ const controlRecipes = async function () {
     const id = window.location.hash.slice(1);
     if (!id) return;
     recipeView.renderSpinner();
+    // 0. Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
     // 1. Loading recipe
     await model.loadRecipe(id);
 
