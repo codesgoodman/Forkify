f3f8094fdc7111749e18f4d08b473592849cda49
Kihyun Kim <rlgus3689@gmail.com>
2023-03-31 14:43:24 +0900

    debugging id

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3252,8 +3252,10 @@ class ResultsView extends (0, _viewJsDefault.default) {
     }
     _generateMarkupPreview(result) {
         const id = window.location.hash.slice(1);
+        console.log(result.id);
+        console.log(id);
         return `<li class="preview">
-      <a class="preview__link ${result.id === id ? "preview__link--active" : " "}" href="#${result.id}">
+      <a class="preview__link ${result.id === id ? "preview__link--active" : ""}" href="#${result.id}">
         <figure class="preview__fig">
           <img src="${result.image}" alt="${result.title}" />
         </figure>
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -9,9 +9,11 @@ class ResultsView extends View {
   }
   _generateMarkupPreview(result) {
     const id = window.location.hash.slice(1);
+    console.log(result.id);
+    console.log(id);
     return `<li class="preview">
       <a class="preview__link ${
-        result.id === id ? 'preview__link--active' : ' '
+        result.id === id ? 'preview__link--active' : ''
       }" href="#${result.id}">
         <figure class="preview__fig">
           <img src="${result.image}" alt="${result.title}" />
