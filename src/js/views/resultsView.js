8ca3b9ecd7ced3adf6204a9cebeee88b0838b4f7
Kihyun Kim <rlgus3689@gmail.com>
2023-04-07 12:59:30 +1200

    run into another error :(

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -11,7 +11,6 @@ class ResultsView extends View {
     const id = window.location.hash.slice(1);
 
     console.log(result);
-    console.log(result.id);
 
     return `<li class="preview">
       <a class="preview__link ${
