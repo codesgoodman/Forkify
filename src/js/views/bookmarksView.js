02d85c38d643e08ac7fb12c90fffc7139ba7e495
Kihyun Kim <rlgus3689@gmail.com>
2023-04-15 23:07:10 +1200

    create bookmarksView and copied the code from resultsView

---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,30 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg';
+class ResultsView extends View {
+  _parentEl = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your specific query. Please Try again!';
+  _message = '';
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
+  _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
+
+    return `<li class="preview">
+      <a class="preview__link ${
+        result.id === id ? 'preview__link--active' : ''
+      }" href="#${result.id}">
+        <figure class="preview__fig">
+          <img src="${result.image}" alt="${result.title}" />
+        </figure>
+        <div class="preview__data">
+          <h4 class="preview__title">${result.title}</h4>
+          <p class="preview__publisher">${result.publisher}</p>
+          
+        </div>
+      </a>
+    </li>`;
+  }
+}
+
+export default new ResultsView();
