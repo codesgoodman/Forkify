6a2b888fd80e799e1e5747a4bf12798b3ee544bb
Kihyun Kim <rlgus3689@gmail.com>
2023-04-19 23:01:53 +1200

    got rid of general, error message

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -2,8 +2,7 @@ import View from './view.js';
 import icons from 'url:../../img/icons.svg';
 class PreviewView extends View {
   _parentEl = document.querySelector('.results');
-  _errorMessage = 'No recipes found for your specific query. Please Try again!';
-  _message = '';
+
   _generateMarkup() {
     return this._data.map(this._generateMarkupPreview).join('');
   }
