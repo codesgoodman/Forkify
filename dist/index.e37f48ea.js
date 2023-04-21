a18e53cb2bacf22e8c88dc633cc9df86dc3d45f0
Kihyun Kim <rlgus3689@gmail.com>
2023-04-21 13:56:35 +1200

    get rid of generateMarkupPreview from bookmarksView

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3372,21 +3372,6 @@ class BookmarksView extends (0, _viewJsDefault.default) {
     _generateMarkup() {
         return this._data.map(this._generateMarkupPreview).join("");
     }
-    _generateMarkupPreview(result) {
-        const id = window.location.hash.slice(1);
-        return `<li class="preview">
-      <a class="preview__link ${result.id === id ? "preview__link--active" : ""}" href="#${result.id}">
-        <figure class="preview__fig">
-          <img src="${result.image}" alt="${result.title}" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
-          
-        </div>
-      </a>
-    </li>`;
-    }
 }
 exports.default = new BookmarksView();
 
