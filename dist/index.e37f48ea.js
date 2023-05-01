dab0e5249af013051a60c72f640e62bd6c2f4e28
Kihyun Kim <rlgus3689@gmail.com>
2023-05-01 22:47:07 +1200

    replace result with this._data in markup

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3390,13 +3390,13 @@ class PreviewView extends (0, _viewJsDefault.default) {
     _generateMarkupPreview(result) {
         const id = window.location.hash.slice(1);
         return `<li class="preview">
-      <a class="preview__link ${result.id === id ? "preview__link--active" : ""}" href="#${result.id}">
+      <a class="preview__link ${this._data.id === id ? "preview__link--active" : ""}" href="#${this._data.id}">
         <figure class="preview__fig">
-          <img src="${result.image}" alt="${result.title}" />
+          <img src="${this._data.image}" alt="${this._data.title}" />
         </figure>
         <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
+          <h4 class="preview__title">${this._data.title}</h4>
+          <p class="preview__publisher">${this._data.publisher}</p>
           
         </div>
       </a>
--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -8,14 +8,14 @@ class PreviewView extends View {
 
     return `<li class="preview">
       <a class="preview__link ${
-        result.id === id ? 'preview__link--active' : ''
-      }" href="#${result.id}">
+        this._data.id === id ? 'preview__link--active' : ''
+      }" href="#${this._data.id}">
         <figure class="preview__fig">
-          <img src="${result.image}" alt="${result.title}" />
+          <img src="${this._data.image}" alt="${this._data.title}" />
         </figure>
         <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
+          <h4 class="preview__title">${this._data.title}</h4>
+          <p class="preview__publisher">${this._data.publisher}</p>
           
         </div>
       </a>
