cb6f1b3a9de2c9810abc5ff10c98a8fb5eae7c19
Kihyun Kim <rlgus3689@gmail.com>
2023-06-02 14:16:35 +1200

    aasdasd

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -3,7 +3,7 @@ import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 class ResultsView extends View {
   _parentEl = document.querySelector('.results');
-  _errorMessage = 'No recipes found for your specific query. Please Try again';
+  _errorMessage = 'No recipes found for your specific query. Please Try again!';
   _message = '';
   _generateMarkup() {
     return this._data.map(result => previewView.render(result, false)).join('');
