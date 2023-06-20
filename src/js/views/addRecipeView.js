088547270808a7514f86b30b6971ab97d5963634
Kihyun Kim <rlgus3689@gmail.com>
2023-06-20 17:30:01 +1200

    add handler on btnOpen to toggle hidden class

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -7,6 +7,13 @@ class addRecipeView extends View {
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.btn--close-modal');
 
+  addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', function () {
+      this._overlay.classList.toggle('.hidden');
+      this._window.classList.toggle('.hidden');
+    });
+  }
+
   _generateMarkup() {}
 }
 
