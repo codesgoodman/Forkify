2ebd0349a84808bfdc011af1de0d50839864db46
Kihyun Kim <rlgus3689@gmail.com>
2023-06-20 17:33:11 +1200

    add a constructor to call handler. Also capitalised class name

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,13 +1,18 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-class addRecipeView extends View {
+class AddRecipeView extends View {
   _parentEl = document.querySelector('.upload');
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.btn--close-modal');
 
-  addHandlerShowWindow() {
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+  }
+
+  _addHandlerShowWindow() {
     this._btnOpen.addEventListener('click', function () {
       this._overlay.classList.toggle('.hidden');
       this._window.classList.toggle('.hidden');
@@ -17,4 +22,4 @@ class addRecipeView extends View {
   _generateMarkup() {}
 }
 
-export default new addRecipeView();
+export default new AddRecipeView();
