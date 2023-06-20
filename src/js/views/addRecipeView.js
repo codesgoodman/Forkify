f630cd3aea479726cafa4d47eb97642dbfbc40f6
Kihyun Kim <rlgus3689@gmail.com>
2023-06-20 17:20:10 +1200

    tidy up the code and change names

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,67 +1,9 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-class PaginationView extends View {
+class addRecipeView extends View {
   _parentEl = document.querySelector('.pagination');
-  addHandlerClick(handler) {
-    this._parentEl.addEventListener('click', function (e) {
-      const btn = e.target.closest('.btn--inline');
-      if (!btn) return;
-      const goToPage = +btn.dataset.goto;
-      handler(goToPage);
-    });
-  }
-  _generateMarkup() {
-    const curPage = this._data.page;
-    const numPages = Math.ceil(
-      this._data.results.length / this._data.resultsPerPage
-    );
 
-    // page 1 and there are more pages
-    if (curPage === 1 && numPages > 1) {
-      return `<button data-goto="${
-        curPage + 1
-      }" class="btn--inline pagination__btn--next">
-      <span>Page ${curPage + 1}</span>
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-right"></use>
-      </svg>
-    </button>`;
-    }
-    // page 1 and it is the only page
-
-    // last page
-    if (curPage === numPages && numPages > 1) {
-      return `<button data-goto="${
-        curPage - 1
-      }" class="btn--inline pagination__btn--prev">
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-left"></use>
-      </svg>
-      <span>Page ${curPage - 1}</span>
-    </button>`;
-    }
-    // other pages
-    if (curPage < numPages) {
-      return `<button data-goto="${
-        curPage - 1
-      }" class="btn--inline pagination__btn--prev">
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-left"></use>
-      </svg>
-      <span>Page ${curPage - 1}</span>
-    </button>
-    <button data-goto="${
-      curPage + 1
-    }" class="btn--inline pagination__btn--next">
-      <span>Page ${curPage + 1}</span>
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-right"></use>
-      </svg>
-    </button>`;
-    }
-    // page 1 and it's the only page
-    return '';
-  }
+  _generateMarkup() {}
 }
 
-export default new PaginationView();
+export default new addRecipeView();
