ae3fd594388c53fae7c604c27f8b75b85421a49b
Kihyun Kim <rlgus3689@gmail.com>
2023-06-20 17:25:34 +1200

    select required components from HTML

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,7 +1,11 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
 class addRecipeView extends View {
-  _parentEl = document.querySelector('.pagination');
+  _parentEl = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
 
   _generateMarkup() {}
 }
