c61162d1b8b74f76eca978c623a6f80533fc90cd
Kihyun Kim <rlgus3689@gmail.com>
2023-06-20 19:44:55 +1200

    convert recipe object into an array and filter out

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2143,7 +2143,8 @@ const clearBookmarks = function() {
     localStorage.clear("bookmarks");
 };
 const uploadRecipe = async function(newRecipe) {
-    console.log(Object.entries(newRecipe));
+    const ingredients = Object.entries(newRecipe).filter((entry)=>entry[0].startsWith("ingredient") && entry[1] !== "");
+    console.log(ingredients);
 };
 
 },{"regenerator-runtime":"dXNgZ","./config.js":"k5Hzs","./helpers.js":"hGI1E","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"dXNgZ":[function(require,module,exports) {
--- src/js/model.js
+++ src/js/model.js
@@ -106,5 +106,8 @@ const clearBookmarks = function () {
 // clearBookmarks();
 
 export const uploadRecipe = async function (newRecipe) {
-  console.log(Object.entries(newRecipe));
+  const ingredients = Object.entries(newRecipe).filter(
+    entry => entry[0].startsWith('ingredient') && entry[1] !== ''
+  );
+  console.log(ingredients);
 };
