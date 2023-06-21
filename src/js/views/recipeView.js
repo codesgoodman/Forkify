6316d68cf63f67fe1f01d55a7acf8437a1e44cd9
Kihyun Kim <rlgus3689@gmail.com>
2023-06-21 20:30:10 +1200

    succesfully complete manual deployment

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './view.js';
 //import icons
 import icons from 'url:../../img/icons.svg'; // parcel v2 needs url: at the front for non-programming files
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
   _errorMessage = 'We could not find the recipe. Please try again!';
@@ -122,7 +122,7 @@ class RecipeView extends View {
     <use href="${icons}#icon-check"></use>
   </svg>
   <div class="recipe__quantity">${
-    ing.quantity ? new Fraction(ing.quantity).toString() : ''
+    ing.quantity ? fracty(ing.quantity).toString() : ''
   }</div>
   <div class="recipe__description">
     <span class="recipe__unit">${ing.unit}</span>
