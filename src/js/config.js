361d453780a9cc11d71de43eb490944588a849f8
Kihyun Kim <rlgus3689@gmail.com>
2023-06-21 19:12:02 +1200

    run npm build to finalise

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'f5da67c9-ff49-48d1-a773-a468b0abb511';
+export const MODAL_CLOSE_SEC = 2.5;
